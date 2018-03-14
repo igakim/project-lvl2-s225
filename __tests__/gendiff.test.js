f56fe72ac5acfcdb74b886112fab0076d9c152c6
Igor Kim <igkim90@gmail.com>
2018-03-14 13:13:04 +0500

    step 4 add ini diffs

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -15,3 +15,10 @@ const afterYAML = '__tests__/__fixtures__/after.yml';
 test('compare two yaml', () => {
   expect(genDiff(beforeYAML, afterYAML)).toBe(fs.readFileSync('__tests__/__fixtures__/expected.txt', 'utf-8'));
 });
+
+const beforeINI = '__tests__/__fixtures__/before.ini';
+const afterINI = '__tests__/__fixtures__/after.ini';
+
+test('compare two ini', () => {
+  expect(genDiff(beforeINI, afterINI)).toBe(fs.readFileSync('__tests__/__fixtures__/expected.txt', 'utf-8'));
+});
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import _ from 'lodash';
 import fs from 'fs';
 import path from 'path';
 import yaml from 'js-yaml';
+import ini from 'ini';
 
 const parsers = {
   '.json': el => JSON.parse(el),
   '.yml': el => yaml.safeLoad(el),
+  '.ini': el => ini.parse(el),
 };
 
 const toString = (key, after, before) => {
@@ -23,8 +25,8 @@ const toString = (key, after, before) => {
 export default (beforePath, afterPath) => {
   const extname = path.extname(beforePath);
   const parse = parsers[extname];
-  const before = fs.readFileSync(beforePath);
-  const after = fs.readFileSync(afterPath);
+  const before = fs.readFileSync(beforePath, 'utf-8');
+  const after = fs.readFileSync(afterPath, 'utf-8');
   const parsedBefore = parse(before);
   const parsedAfter = parse(after);
   const united = _.union(_.keys(parsedBefore), _.keys(parsedAfter));
