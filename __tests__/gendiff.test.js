864f4807b79b7453ae12b5c895f52f829a42aaca
Igor Kim <igkim90@gmail.com>
2018-03-14 12:49:26 +0500

    add yaml diffs

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,14 +1,11 @@
 import fs from 'fs';
 import genDiff from '../src';
 
-
-const expected = fs.readFileSync('__tests__/__fixtures__/expected.txt', 'utf-8');
-
 const beforeJSON = '__tests__/__fixtures__/before.json';
 const afterJSON = '__tests__/__fixtures__/after.json';
 
 test('compare two json', () => {
-  expect(genDiff(beforeJSON, afterJSON)).toBe(expected);
+  expect(genDiff(beforeJSON, afterJSON)).toBe(fs.readFileSync('__tests__/__fixtures__/expected.txt', 'utf-8'));
 });
 
 
@@ -16,5 +13,5 @@ const beforeYAML = '__tests__/__fixtures__/before.yml';
 const afterYAML = '__tests__/__fixtures__/after.yml';
 
 test('compare two yaml', () => {
-  expect(genDiff(beforeYAML, afterYAML).toBe(expected));
+  expect(genDiff(beforeYAML, afterYAML)).toBe(fs.readFileSync('__tests__/__fixtures__/expected.txt', 'utf-8'));
 });
--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import _ from 'lodash';
 import fs from 'fs';
+import path from 'path';
+import yaml from 'js-yaml';
+
+const parsers = {
+  '.json': el => JSON.parse(el),
+  '.yml': el => yaml.safeLoad(el),
+};
 
 const toString = (key, after, before) => {
   if (after[key] === before[key]) {
@@ -12,14 +19,15 @@ const toString = (key, after, before) => {
   return `  + ${key}: ${after[key]}\n  - ${key}: ${before[key]}\n`;
 };
 
+
 export default (beforePath, afterPath) => {
+  const extname = path.extname(beforePath);
+  const parse = parsers[extname];
   const before = fs.readFileSync(beforePath);
   const after = fs.readFileSync(afterPath);
-  const parsedBefore = JSON.parse(before);
-  const parsedAfter = JSON.parse(after);
-  const beforeKeys = _.keys(parsedBefore);
-  const afterKeys = _.keys(parsedAfter);
-  const concatedKeys = beforeKeys.concat(afterKeys);
-  const unique = new Set(concatedKeys);
+  const parsedBefore = parse(before);
+  const parsedAfter = parse(after);
+  const united = _.union(_.keys(parsedBefore), _.keys(parsedAfter));
+  const unique = new Set(united);
   return `{\n${Array.from(unique).map(el => toString(el, parsedAfter, parsedBefore)).join('')}}`;
 };
